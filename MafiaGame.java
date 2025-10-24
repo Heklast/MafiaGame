057b37bbc93789a39a105b5d9cbfc52a73621625
Hekla <heklast@gmail.com>
2025-10-24 15:14:21 +0100

    Character files

--- MafiaGame.java
+++ MafiaGame.java
@@ -15,6 +15,9 @@ public class MafiaGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cop cop=new Cop();
+        Human human =new Human();
+        PoliceChief policeChief=new PoliceChief();
     }
     
 }
